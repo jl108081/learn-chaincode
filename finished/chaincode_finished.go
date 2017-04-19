8f056b7a9ad26a291633ded568da87b8c7a7dc6b
Zhanyi <zhanyihuw@hotmail.com>
2017-04-19 12:54:05 +0200

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -130,23 +130,3 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 
 	return valAsbytes, nil
 }
-func (t *SimpleChaincode) read2(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, key2, jsonResp string
-	var err error
-
-	if len(args) != 3 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-	}
-
-	key = args[0]
-	key2 = args[1]
-	valAsbytes, err := stub.GetState(key)
-	valAsbytes, err := stub.GetState(key2)
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	return valAsbytes, nil
-}
-
